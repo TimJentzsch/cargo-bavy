9c73bfb030ce7540c18c641d9949c982ba92829b
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-08-16 21:36:27 +0200

    Add possibility to create new cargo project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{
+    env,
+    io::{self, Write},
+    process::Command,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let folder_name = env::args()
+        .nth(1)
+        .expect("Please specify the name of the project.");
+
+    let output = Command::new("cargo")
+        .arg("new")
+        .arg(folder_name)
+        .output()
+        .expect("Failed to create the new project.");
+
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
 }
