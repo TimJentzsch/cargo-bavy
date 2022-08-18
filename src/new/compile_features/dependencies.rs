f10c3b1386d252b881079f6d8f4e2dfb6f265057
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-08-19 00:43:32 +0200

    Fix typos in dependency optimization keys

--- src/new/compile_features/dependencies.rs
+++ src/new/compile_features/dependencies.rs
@@ -17,12 +17,12 @@ fn set_cargo_toml_dependency_optimizations(folder_name: &str) {
 
     // Enable a small amount of optimization in debug mode
     let mut dev_profile = Table::new();
-    dev_profile.insert("opt_level", value(1));
+    dev_profile.insert("opt-level", value(1));
     cargo_toml.insert("profile.dev", Item::Table(dev_profile));
 
     // Enable high optimizations for dependencies (incl. Bevy)
     let mut dependency_dev_profile = Table::new();
-    dependency_dev_profile.insert("opt_level", value(3));
+    dependency_dev_profile.insert("opt-level", value(3));
     cargo_toml.insert(
         r#"profile.dev.package."*""#,
         Item::Table(dependency_dev_profile),
