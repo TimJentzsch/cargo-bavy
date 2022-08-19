6ccf888e8659152bda67e41ca806c1e9d7c45eed
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-08-19 13:57:24 +0200

    Implement basic cargo bavy run functionality

--- src/run/mod.rs
+++ src/run/mod.rs
@@ -1,3 +1,16 @@
+use std::process::{exit, Command};
+
 pub mod cli;
 
-pub fn run() {}
+pub fn run() {
+    let status = Command::new("cargo")
+        .arg("run")
+        .arg("--features")
+        .arg("bevy/dynamic")
+        .status()
+        .expect("Failed to run `cargo run`.");
+
+    if !status.success() {
+        exit(status.code().unwrap_or(1));
+    }
+}
