74cf85bd25b65a575b4417cacd8e54150000cb71
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-08-19 14:46:03 +0200

    Add more options to cargo bavy run

--- src/run/cli.rs
+++ src/run/cli.rs
@@ -5,10 +5,34 @@ use crate::cli::Command;
 use super::run;
 
 #[derive(Debug, Args)]
-pub struct RunCommand {}
+pub struct RunCommand {
+    /// Name of the bin target to run.
+    #[clap(long = "bin", value_name = "NAME")]
+    pub bin: Option<String>,
+
+    /// Name of the example target to run.
+    #[clap(long = "example", value_name = "NAME")]
+    pub example: Option<String>,
+
+    /// Build artifacts in release mode, with optimizations.
+    #[clap(short = 'r', long = "release", default_value_t = false)]
+    pub is_release: bool,
+
+    /// Build for the target triple.
+    #[clap(long = "target", value_name = "TRIPLE")]
+    pub target: Option<String>,
+
+    /// Directory for all generated artifacts.
+    #[clap(long = "target-dir", value_name = "DIRECTORY")]
+    pub target_dir: Option<String>,
+
+    /// Path to Cargo.toml.
+    #[clap(long = "manifest-path", value_name = "PATH")]
+    pub manifest_path: Option<String>,
+}
 
 impl Command for RunCommand {
     fn exec(&self) {
-        run();
+        run(self);
     }
 }
--- src/run/mod.rs
+++ src/run/mod.rs
@@ -1,12 +1,83 @@
 use std::process::{exit, Command};
 
+use self::cli::RunCommand;
+
 pub mod cli;
 
-pub fn run() {
+struct CargoArgs(Vec<String>);
+
+impl CargoArgs {
+    fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    /// Add an argument.
+    ///
+    /// Can be a `&str` or a `String`.
+    /// If it contains whitespace, it is split into multiple args.
+    fn add<A>(&mut self, arg: A)
+    where
+        A: Into<String>,
+    {
+        let arg: String = arg.into();
+
+        for part in arg.split_ascii_whitespace() {
+            self.0.push(part.to_string());
+        }
+    }
+
+    /// Add an argument with a value.
+    fn add_with_value<A, V>(&mut self, arg: A, value: V)
+    where
+        A: Into<String>,
+        V: Into<String>,
+    {
+        self.add(arg);
+        self.add(value);
+    }
+}
+
+pub fn run(args: &RunCommand) {
+    let mut cargo_args = CargoArgs::new();
+
+    // --bin <NAME>
+    if let Some(name) = &args.bin {
+        cargo_args.add_with_value("--bin", name);
+    }
+
+    // --example <NAME>
+    if let Some(name) = &args.example {
+        cargo_args.add_with_value("--example", name);
+    }
+
+    // --release
+    if args.is_release {
+        cargo_args.add("--release");
+    }
+
+    // --target <TRIPLE>
+    if let Some(triple) = &args.target {
+        cargo_args.add_with_value("--target", triple);
+    }
+
+    // --target-dir <DIRECTORY>
+    if let Some(directory) = &args.target_dir {
+        cargo_args.add_with_value("--target-dir", directory);
+    }
+
+    // --manifest-path <PATH>
+    if let Some(path) = &args.manifest_path {
+        cargo_args.add_with_value("--manifest-path", path);
+    }
+
+    // Enable dynamic linking if not in release mode
+    if !args.is_release {
+        cargo_args.add_with_value("--features", "bevy/dynamic");
+    }
+
     let status = Command::new("cargo")
         .arg("run")
-        .arg("--features")
-        .arg("bevy/dynamic")
+        .args(cargo_args.0)
         .status()
         .expect("Failed to run `cargo run`.");
 
