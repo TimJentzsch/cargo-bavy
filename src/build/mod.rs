77a7989cf7ddb4084fa06f32be68c6513c5d0e55
TimJentzsch <tim-jentzsch@gmx.de>
2022-08-20 22:25:53 +0200

    Add `cargo bavy build` command (#13)
    * Add cargo bavy build command
    
    * Add additional arguments for cargo bavy build
    
    * Add new builder methods to ArgsBuilder
    
    * Refactor args building for cargo bavy build
    
    * Refactor args building for cargo bavy run
    
    * Pass additional args to cargo with cargo bavy build
    
    * Update changelog with cargo bavy build

--- src/build/mod.rs
+++ src/build/mod.rs
@@ -1,19 +1,18 @@
 mod args;
 pub mod cli;
 
-use dialoguer::console::{style, Style};
+use dialoguer::console::Style;
 
 use crate::{
-    cargo::{cargo_build, cargo_run, ArgBuilder},
+    cargo::{cargo_build, ArgBuilder},
     files::replace_folder,
-    http_server::launch_game,
     rustup::install_target_if_needed,
     wasm_bindgen::{bundle_to_web, create_wasm_folder_if_needed, install_wasm_bindgen_if_needed},
 };
 
-use self::cli::RunCommand;
+use self::cli::BuildCommand;
 
-pub fn run(args: &RunCommand) {
+pub fn build(args: &BuildCommand) {
     let info_style = Style::new().magenta().dim();
 
     if args.is_wasm {
@@ -40,14 +39,7 @@ pub fn run(args: &RunCommand) {
 
         println!("{}", info_style.apply_to("Updating assets..."));
         replace_folder("assets", "wasm").expect("Failed to update asset folder");
-
-        println!("{}", info_style.apply_to("Serving on localhost..."));
-        println!(
-            "Open your game at <{}>.",
-            style("http://127.0.0.1:4000").green()
-        );
-        launch_game().expect("Failed to launch game");
     } else {
-        cargo_run(cargo_args);
+        cargo_build(cargo_args);
     }
 }
