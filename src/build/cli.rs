24d768e1dd7eb603b241abbd77c0b612a483ca76
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-08-20 21:08:04 +0200

    Add additional arguments for cargo bavy build

--- src/build/cli.rs
+++ src/build/cli.rs
@@ -6,13 +6,61 @@ use super::build;
 
 #[derive(Debug, Args)]
 pub struct BuildCommand {
+    /// Package to build (see `cargo help pkgid`).
+    #[clap(short = 'p', long = "package", value_name = "SPEC")]
+    pub package: Option<String>,
+
+    /// Build all packages in the workspace.
+    #[clap(long = "workspace", action = ArgAction::SetTrue, default_value_t = false)]
+    pub is_workspace: bool,
+
+    ///  Exclude packages from the build.
+    #[clap(long = "exclude", value_name = "SPEC")]
+    pub exclude: Option<String>,
+
+    /// Build only this package's library.
+    #[clap(long = "lib", action = ArgAction::SetTrue, default_value_t = false)]
+    pub is_lib: bool,
+
+    /// Require Cargo.lock is up to date.
+    #[clap(long = "locked", action = ArgAction::SetTrue, default_value_t = false)]
+    pub is_locked: bool,
+
     /// Build only the specified binary.
     #[clap(long = "bin", value_name = "NAME")]
     pub bin: Option<String>,
 
+    /// Build all binaries.
+    #[clap(long = "bins", action = ArgAction::SetTrue, default_value_t = false)]
+    pub is_bins: bool,
+
     /// Build only the specified example.
     #[clap(long = "example", value_name = "NAME")]
     pub example: Option<String>,
+
+    /// Build all examples.
+    #[clap(long = "examples", action = ArgAction::SetTrue, default_value_t = false)]
+    pub is_examples: bool,
+
+    /// Build only the specified test target.
+    #[clap(long = "test", value_name = "NAME")]
+    pub test: Option<String>,
+
+    /// Build all tests.
+    #[clap(long = "tests", action = ArgAction::SetTrue, default_value_t = false)]
+    pub is_tests: bool,
+
+    /// Build only the specified bench target.
+    #[clap(long = "bench", value_name = "NAME")]
+    pub bench: Option<String>,
+
+    /// Build all benches.
+    #[clap(long = "benches", action = ArgAction::SetTrue, default_value_t = false)]
+    pub is_benches: bool,
+
+    /// Build all benches.
+    #[clap(long = "all-targets", action = ArgAction::SetTrue, default_value_t = false)]
+    pub is_all_targets: bool,
 
     /// Build artifacts in release mode, with optimizations.
     #[clap(short = 'r', long = "release", action = ArgAction::SetTrue, default_value_t = false)]
