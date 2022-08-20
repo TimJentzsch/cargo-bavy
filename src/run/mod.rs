967c8304696718b08aa68f48df11a3e26cd5aabe
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-08-20 21:56:14 +0200

    Refactor args building for cargo bavy run

--- src/run/mod.rs
+++ src/run/mod.rs
@@ -1,3 +1,6 @@
+mod args;
+pub mod cli;
+
 use dialoguer::console::{style, Style};
 
 use crate::{
@@ -10,10 +13,7 @@ use crate::{
 
 use self::cli::RunCommand;
 
-pub mod cli;
-
 pub fn run(args: &RunCommand) {
-    let mut cargo_args = ArgBuilder::new();
     let info_style = Style::new().magenta().dim();
 
     if args.is_wasm {
@@ -29,43 +29,7 @@ pub fn run(args: &RunCommand) {
         create_wasm_folder_if_needed(true).expect("Creation of `wasm/` folder failed.");
     }
 
-    // --bin <NAME>
-    if let Some(name) = &args.bin {
-        cargo_args.add_with_value("--bin", name);
-    }
-
-    // --example <NAME>
-    if let Some(name) = &args.example {
-        cargo_args.add_with_value("--example", name);
-    }
-
-    // --release
-    if args.is_release {
-        cargo_args.add("--release");
-    }
-
-    // --wasm / --target <TRIPLE>
-    if args.is_wasm {
-        // --wasm takes precedence
-        cargo_args.add_with_value("--target", "wasm32-unknown-unknown");
-    } else if let Some(triple) = &args.target {
-        cargo_args.add_with_value("--target", triple);
-    }
-
-    // --target-dir <DIRECTORY>
-    if let Some(directory) = &args.target_dir {
-        cargo_args.add_with_value("--target-dir", directory);
-    }
-
-    // --manifest-path <PATH>
-    if let Some(path) = &args.manifest_path {
-        cargo_args.add_with_value("--manifest-path", path);
-    }
-
-    // Enable dynamic linking if not in release mode and not building for WASM
-    if !args.is_release && !args.is_wasm {
-        cargo_args.add_with_value("--features", "bevy/dynamic");
-    }
+    let cargo_args = ArgBuilder::from(args);
 
     if args.is_wasm {
         println!("{}", info_style.apply_to("Building for WASM..."));
